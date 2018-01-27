51ab70850e6377edf49481b59f42ea153c6a3204
leesper <pascal7718@gmail.com>
2018-01-27 19:09:48 +0800

    finish P2

---
+++ assign2_p2.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+struct Employee {
+  string firstName;
+  string lastName;
+  string ssn;
+  double wage;
+  int hours;
+  bool isFullTime;
+};
+
+Employee parseEmployee(string line) {
+  Employee emp;
+  string wageStr, hoursStr, status;
+  stringstream ss(line);
+  getline(ss, emp.firstName, ',');
+  getline(ss, emp.lastName, ',');
+  getline(ss, emp.ssn, ',');
+  getline(ss, wageStr, ',');
+  getline(ss, hoursStr, ',');
+  getline(ss, status, ',');
+  emp.wage = atof(wageStr.c_str());
+  emp.hours = atoi(hoursStr.c_str());
+  emp.isFullTime = status == "F" ? true : false;
+  return emp;
+}
+
+vector<Employee> parseCSV(string csvFile) {
+  vector<Employee> emps;
+  ifstream ifs(csvFile.c_str());
+  int lineno = 0;
+  if (ifs.is_open()) {
+    string line;
+    while (getline(ifs, line)) {
+      emps.push_back(parseEmployee(line));
+    }
+  } else {
+    cout << "file " << csvFile << " not found" << endl;
+  }
+  return emps;
+}
+
+void report(vector<Employee> employees) {
+  cout << "Name\t" << "SSN\t" << "Wage\t" << "Hours\t" << "Straight\t" << "Overtime\t" << "Deductions\t" << "Status\t" << "Net" << endl;
+  for (vector<Employee>::iterator it = employees.begin(); it != employees.end(); ++it) {
+    string name = it->firstName + " " + it->lastName;
+    double straight = it->hours <= 40 ? it->wage * it->hours : it->wage * 40;
+    double overtime = it->hours <= 40 ? 0.0 : (it->hours - 40) * it->wage * 1.5;
+    int deductions = it->isFullTime ? 10 : 0;
+    string status = it->isFullTime ? "Full Time" : "Part Time";
+    double net = straight + overtime - deductions;
+    cout << name << '\t'
+         << it->ssn << '\t'
+         << it->wage << '\t'
+         << it->hours << '\t'
+         << straight << '\t'
+         << overtime << '\t'
+         << deductions << '\t'
+         << status << '\t'
+         << net << endl;
+  }
+}
+
+int main() {
+  // parse employee info from csv file
+  vector<Employee> employees = parseCSV("employee.csv");
+  // report output info
+  report(employees);
+
+}
