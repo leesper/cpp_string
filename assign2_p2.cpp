4fd518dd376b928e594d40d265ee5967811feeed
leesper <pascal7718@gmail.com>
2018-01-27 19:23:00 +0800

    add comments

--- assign2_p2.cpp
+++ assign2_p2.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 using namespace std;
 
+// the employee info
 struct Employee {
   string firstName;
   string lastName;
@@ -15,22 +16,26 @@ struct Employee {
   bool isFullTime;
 };
 
+// parse one line of csv, construct employee struct
 Employee parseEmployee(string line) {
   Employee emp;
   string wageStr, hoursStr, status;
   stringstream ss(line);
+  // get all fields separated by comma
   getline(ss, emp.firstName, ',');
   getline(ss, emp.lastName, ',');
   getline(ss, emp.ssn, ',');
   getline(ss, wageStr, ',');
   getline(ss, hoursStr, ',');
   getline(ss, status, ',');
+  // convert string into double and int
   emp.wage = atof(wageStr.c_str());
   emp.hours = atoi(hoursStr.c_str());
   emp.isFullTime = status == "F" ? true : false;
   return emp;
 }
 
+// parse CSV file and construct employees
 vector<Employee> parseCSV(string csvFile) {
   vector<Employee> emps;
   ifstream ifs(csvFile.c_str());
@@ -47,7 +52,9 @@ vector<Employee> parseCSV(string csvFile) {
 }
 
 void report(vector<Employee> employees) {
+  // display headers
   cout << "Name\t" << "SSN\t" << "Wage\t" << "Hours\t" << "Straight\t" << "Overtime\t" << "Deductions\t" << "Status\t" << "Net" << endl;
+  // iterate employees, calculate and display all infos.
   for (vector<Employee>::iterator it = employees.begin(); it != employees.end(); ++it) {
     string name = it->firstName + " " + it->lastName;
     double straight = it->hours <= 40 ? it->wage * it->hours : it->wage * 40;
